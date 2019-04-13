1191e8a06a70f70fd4e48250f0af54fa92c0c7aa
Marzena <kacprowicz.marzena@gmail.com>
2019-04-13 18:20:10 +0200

    "Person-form.jsp renamed to login-form.jsp"

---
+++ src/main/java/pl/coderslab/app/person/PersonDetailsController.java
@@ -0,0 +1,9 @@
+package pl.coderslab.app.person;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/person-details")
+public class PersonDetailsController {
+}
