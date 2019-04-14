8feb6a6cd775ac45f4c9c2684af56d62e7137f13
Marzena <kacprowicz.marzena@gmail.com>
2019-04-14 11:48:38 +0200

    "Author class updated: @NotNull validation added to firsName and lastName, Pesel and Email fields with validation added."

--- src/main/java/pl/coderslab/app/author/Author.java
+++ src/main/java/pl/coderslab/app/author/Author.java
@@ -1,8 +1,11 @@
 package pl.coderslab.app.author;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.pl.PESEL;
 import pl.coderslab.app.book.Book;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +15,21 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
 
     @ManyToMany
     private List<Book> books = new ArrayList<>();
+
+    @PESEL
+    private Integer pesel;
+
+    @Email
+    private String email;
 
     public Author() {
     }
